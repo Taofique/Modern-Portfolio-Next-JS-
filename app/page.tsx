3d0eb25283a1d0b6434c51fba04ab3787f1d51e1
Taofique <shornob8@gmail.com>
2025-09-12 22:15:25 +0600

    feat: Implemented the full hero section, hover nav bar and bento grid from Aceternity UI library and improved color coding

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,22 @@
 import Hero from "@/components/Hero";
 import TestComponent from "@/components/TestComponents";
+import { FloatingNav } from "@/components/ui/FloatingNav";
+import { FaHome } from "react-icons/fa";
+import Grid from "@/components/Grid";
 
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full ">
+        <FloatingNav
+          className=""
+          navItems={[
+            { name: "Home", link: "/", icon: <FaHome /> },
+            { name: "Test", link: "/test", icon: <TestComponent /> },
+          ]}
+        />
         <Hero />
+        <Grid />
       </div>
     </main>
   );
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,6 +1,9 @@
 import { cn } from "@/utils/cn";
 import React from "react";
 import { Spotlight } from "./ui/Spotlight";
+import { TextGenerateEffect } from "./ui/TextGenerateEffect";
+import { MagicButton } from "./ui/MagicButton";
+import { FaLocationArrow } from "react-icons/fa";
 
 const Hero = () => {
   return (
@@ -37,6 +40,23 @@ const Hero = () => {
           <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
             Dynamic Web Magic with Next.js
           </h2>
+
+          <TextGenerateEffect
+            className="text-center text-[40px] md:text-5xl lg:text-6xl"
+            words="Transforming Concepts into Seamless User Experiences"
+          />
+
+          <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
+            Hi, I&apos;m Taofique, a JavaScript Developer based in Bangladesh.
+          </p>
+
+          <a href="#about">
+            <MagicButton
+              title="Show my work"
+              icon={<FaLocationArrow />}
+              position="right"
+            />
+          </a>
         </div>
       </div>
     </div>
